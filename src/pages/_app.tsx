6b84f931cc5b5b55ae02a93a5352b2ae27a932e3
Pedro Cruz <santahelia.pedro@gmail.com>
2023-05-02 23:20:23 -0300

    Create project and config styles and header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global';
+import type { AppProps } from 'next/app';
+import Image from 'next/image';
+import logoShop from '../assets/Logo.svg';
+import { Container, Header } from '@/styles/pages/app';
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container>
+      <Header>
+        <Image src={logoShop} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
