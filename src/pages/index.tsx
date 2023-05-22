90722fbceca0f18a3df927df6f6a5587a0fd8b0b
Pedro Cruz <54217967+Hyakutak@users.noreply.github.com>
2023-05-22 16:55:39 -0300

    Create page product

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,21 @@
 import { stripe } from "@/lib/stripe";
 import { MainContainer, CardProduct } from '@/styles/pages/index';
-import { GetServerSideProps } from "next";
+import { GetStaticProps  } from "next";
 import Image from "next/image";
 import Stripe from "stripe";
 import { useKeenSlider } from 'keen-slider/react'
 import 'keen-slider/keen-slider.min.css'
 
-interface Product {
-  id: string,
-  name: string,
-  imageUrl: string,
-  price: number
+interface TypeProducts {
+  products: {
+    id: string,
+    name: string,
+    imageUrl: string,
+    price: string
+  }[]
 }
 
-interface TypeProduct {
-  products: Product[]
-}
-
-export default function Home({ products }: TypeProduct) {
+export default function Home({ products }: TypeProducts) {
   const [sliderRef] = useKeenSlider({
     slides: {
       perView: 2.8
@@ -27,7 +25,7 @@ export default function Home({ products }: TypeProduct) {
   return (
     <>
       <MainContainer ref={sliderRef} className="keen-slider">
-        {products.map((product: Product) => {
+        {products.map((product) => {
           return (
             <CardProduct key={product.id} className="keen-slider__slide">
               <Image src={product.imageUrl} width={520} height={480} alt=""/>
@@ -43,26 +41,32 @@ export default function Home({ products }: TypeProduct) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps  = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price']
   });
   
   const products = response.data.map((product) => {
-    const price = product.default_price as Stripe.Price;
+    const priceStripe = product.default_price as Stripe.Price;
+    const price = priceStripe.unit_amount ? priceStripe.unit_amount / 100 : 0;
+    const formatPrice = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    }).format(price);
 
     return {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount,
+      price: formatPrice,
     }
   });
 
   return {
     props: {
       products
-    }
+    },
+    revalidate: 60 * 60 * 2 // 2 hours,
   }
 
 };
